01c5c906aff9128b734f400c4ef23c947028aa02
Utku Erdemir <56741781+UtkuErdemir@users.noreply.github.com>
2021-12-28 23:03:00 +0300

    Update test_pact.py

--- test_pact.py
+++ test_pact.py
@@ -1,7 +1,7 @@
 from pact.verifier import Verifier
 
 
-def test_todo_service_provider_against_pact():
+def todo_service_provider_against_pact():
     verifier = Verifier(provider='todo-app-provider', provider_base_url="http://localhost:5000")
     output, logs = verifier.verify_pacts('./pacts/todo-app-consumer-todo-app-provider.json')
     print(output)
