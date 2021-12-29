f2db775bcd90db08f6c609e729fc4bb53333bbf8
Utku Erdemir <56741781+UtkuErdemir@users.noreply.github.com>
2021-12-29 20:25:30 +0300

    Config changed

--- test_pact.py
+++ test_pact.py
@@ -1,8 +1,5 @@
 from pact.verifier import Verifier
 
-
-def todo_service_provider_against_pact():
-    verifier = Verifier(provider='todo-app-provider', provider_base_url="http://localhost:5000")
-    output, logs = verifier.verify_pacts('./pacts/todo-app-consumer-todo-app-provider.json')
-    print(output)
-    assert output == 0
+verifier = Verifier(provider='todo-app-provider', provider_base_url="http://localhost:5000")
+output, logs = verifier.verify_pacts('./pacts/todo-app-consumer-todo-app-provider.json')
+assert output == 0
